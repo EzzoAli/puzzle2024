87951616f8203b2594c0f522cf663e61a7845e59
Your Name <alez001@student.kristiania.no>
2024-10-03 16:17:53 +0200

    UsrServRep and parent Pom updated

--- user-service/src/main/java/example/com/repository/UserServiceRepository.java
+++ user-service/src/main/java/example/com/repository/UserServiceRepository.java
@@ -1,4 +1,12 @@
 package example.com.repository;
 
-public class UserServiceRepository {
+import example.com.model.UserServiceModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserServiceRepository extends JpaRepository<UserServiceModel, Long> {
+    Optional<UserServiceModel> findByEmail(String email);
 }
