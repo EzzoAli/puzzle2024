9cd9160c2b7b27ba6b0e62ba8a86e9ad9c435c7b
Your Name <alez001@student.kristiania.no>
2024-10-03 14:56:47 +0200

    Initial commit

---
+++ src/test/java/example/com/escape_room/EscapeRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package example.com.escape_room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscapeRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
