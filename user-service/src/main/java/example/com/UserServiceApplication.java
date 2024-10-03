4cb1642f7dab45cf4a1d65f62ed33c293905e19f
Your Name <alez001@student.kristiania.no>
2024-10-03 17:00:51 +0200

    UserServiceApplication bin connected

--- user-service/src/main/java/example/com/UserServiceApplication.java
+++ user-service/src/main/java/example/com/UserServiceApplication.java
@@ -1,4 +1,19 @@
 package example.com;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
 public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
